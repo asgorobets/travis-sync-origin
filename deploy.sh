d43b1b0e7f81ce83a2a476884319bac25fbbd483
Alexei Gorobets <asgorobets@gmail.com>
2016-01-12 00:51:37 +0200

    Reformat .travis.yml and fix minor issues

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy_key # this key should have push access
 ssh-add .travis/deploy_key
 git remote add upstream https://github.com/asgorobets/travis-sync-upstream.git
-git push upstream $TRAVIS_BRANCH
+git push upstream ${TRAVIS_BRANCH}
